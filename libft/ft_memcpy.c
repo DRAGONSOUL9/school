f42513da7e2753f2f22d259864457e23fa3a11d7
dragongit <oussamagit@local.local>
2025-08-12 13:16:33 +0100

    added ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,25 @@
 /*   By: oussama <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 12:21:08 by oussama           #+#    #+#             */
-/*   Updated: 2025/08/12 12:35:15 by oussama          ###   ########.fr       */
+/*   Updated: 2025/08/12 13:15:14 by oussama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *memcpy(void *dest, const void *src,size_t n)
+void	*ft_memcpy(void *dest, const void *src,size_t n)
 {
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char *s;
 
+	i = 0;
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
 }
