4c8fccfe011e59c55b179fcf0d7ad227ba477439
dragongit <oussamagit@local.local>
2025-09-17 03:11:41 +0100

    git files

---
+++ libft/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oussama <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/31 16:26:05 by oussama           #+#    #+#             */
+/*   Updated: 2025/09/12 18:36:50 by oussama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdint.h>
+
+void	test_part_1(void);
+void	test_part_2(void);
+void	test_partbonus(void);
+
+int	main(void)
+{
+	//test_part_1();
+	//test_part_2();
+	test_partbonus();
+	
+	return (0);
+}
